8127d255948e8174376664d5faa186dfd7ec7cf9
Choirul Nurrohim <44932941+cnurrohim@users.noreply.github.com>
2021-04-18 18:32:36 +0700

    refactor menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,5 +1,46 @@
 export function createNav(activeMenu){
-	const menus = [
+	const listMenu = getListMenu();
+	const navTitle = getNavTitle();
+	const navMenu = getNavMenu(listMenu,activeMenu);
+	const barToggleMenu = getbarToggleMenu();
+
+	setNavigationPane(listMenu,navTitle,navMenu,barToggleMenu);
+}
+
+function setNavigationPane(listMenu,navTitle,navMenu,barToggleMenu){
+	const nav = document.querySelector('nav');
+	nav.className = "shift-left cl_secondary";
+	nav.appendChild(navTitle);
+	nav.appendChild(navMenu);
+	document.body.appendChild(barToggleMenu);
+}
+
+function getNavTitle(){
+	const navTitle = document.createElement('h2');
+	navTitle.innerHTML = 'Pemetaan & Prediksi Tingkat Kesehatan Masyarakat di Wilayah Jawa Timur';
+	navTitle.className = "font_primary";
+
+	return navTitle;
+}
+
+function getNavMenu(listMenu,activeMenu){
+	const nav_ul = document.createElement('ul');
+	const navMenu = createLis(listMenu,nav_ul,activeMenu,'main');
+
+	return navMenu;
+}
+
+function getbarToggleMenu(){
+	const barToggleMenu = document.createElement('span');
+	barToggleMenu.addEventListener('click',toggleMenu,false);
+	barToggleMenu.className = "fas fa-bars icon-menu font_accents cl_accents";
+	barToggleMenu.setAttribute('id','mobile-menu');
+	barToggleMenu.innerHTML = "";
+	return barToggleMenu;
+}
+
+function getListMenu(){
+	const listMenu = [
 		{
 			href:'../Training/overview.html',
 			class:'',
@@ -19,27 +60,8 @@ export function createNav(activeMenu){
 			icon:'fas fa-book-reader icon-menu',
 		},
 	];
-	
 
-	const nav = document.querySelector('nav');
-	nav.className = "shift-left cl_secondary";
-	nav.innerHTML = '';
-	const nav_h2 = document.createElement('h2');
-	nav_h2.innerHTML = 'Pemetaan & Prediksi Tingkat Kesehatan Masyarakat di Wilayah Jawa Timur';
-	nav_h2.className = "font_primary";
-	nav.appendChild(nav_h2);
-
-	const nav_ul = document.createElement('ul');
-	const nav_ul_li = createLis(menus,nav_ul,activeMenu,'main');
-	nav.appendChild(nav_ul_li);
-
-	const toggleNav = document.createElement('span');
-	toggleNav.addEventListener('click',toggleMenu,false);
-	toggleNav.className = "fas fa-bars icon-menu font_accents cl_accents";
-	toggleNav.setAttribute('id','mobile-menu');
-	toggleNav.innerHTML = "";
-	document.body.appendChild(toggleNav);
-
+	return listMenu;
 }
 
 /*
@@ -49,55 +71,54 @@ export function createNav(activeMenu){
 	activeMenu: String (nama Menu)
 	activeClass: class-name
 */
-export function createLis(menus,nav_ul,activeMenu,activeClass){
-	let nav_li = {};
-	let a = {};
+export function createLis(listMenu,nav_ul,activeMenu,activeClass){
 	let active = '';
-	let icon = {};
-	menus.forEach((menu)=>{
 
-		icon = document.createElement('i');
+	listMenu.forEach((menu)=>{
+		const a = document.createElement('a');
+		const icon = document.createElement('i');
 		icon.className = menu.icon;
-
-		nav_li = document.createElement('li');
-		a = document.createElement('a');
 		a.innerHTML = menu.menu;
-		a.className = "font_pairs";
-		
-
-		if(menu.class.length > 0){
-			a.setAttribute('class',menu.class);
-		}
-		(menu.menu == activeMenu) ? a.classList.add(activeClass) : '' ;
-		a.setAttribute('href',menu.href);
-		
-		a.prepend(icon);
-		nav_li.appendChild(a);
-		nav_ul.appendChild(nav_li);
+		setLinkMenuAttribute(a,menu,activeMenu,activeClass);
+		appendLinkToNavigationPane(a,icon,nav_ul);
 	});
 
 	return nav_ul;
 }
 
-export function renderMenu(ulContainer,menus){
-	const sub_menu = document.querySelector(ulContainer.ulSelector);
+function appendLinkToNavigationPane(a,icon,nav_ul){
+	const nav_li = document.createElement('li');
+	a.prepend(icon);
+	nav_li.appendChild(a);
+	nav_ul.appendChild(nav_li);
+}
 
-	
+function setLinkMenuAttribute(a,menu,activeMenu,activeClass){
+	a.className = "font_pairs";
+	if(menu.class.length > 0){
+		a.setAttribute('class',menu.class);
+	}
+	(menu.menu == activeMenu) ? a.classList.add(activeClass) : '' ;
+	a.setAttribute('href',menu.href);
+}
+
+export function renderMenu(ulContainer,listMenu){
+	const sub_menu = document.querySelector(ulContainer.ulSelector);
 
 	const nav_ul = document.createElement('ul');
 	nav_ul.setAttribute('class',ulContainer.class);
 	
-	const ulMenu = createLis(menus,nav_ul,ulContainer.activeMenu,ulContainer.activeClass);
-	const menuInfo = renderInfo(ulContainer.activeMenu,menus);
+	const ulMenu = createLis(listMenu,nav_ul,ulContainer.activeMenu,ulContainer.activeClass);
+	const menuInfo = renderInfo(ulContainer.activeMenu,listMenu);
 	sub_menu.appendChild(menuInfo);
 	sub_menu.appendChild(ulMenu);
 }
 
-function renderInfo(activeMenu,menus){
+function renderInfo(activeMenu,listMenu){
 	const menuInfo = document.createElement('div');
 	menuInfo.setAttribute('class','menu-info font_pairs ft_size_detail');
 
-	const selectedMenu = menus.find((menu)=>{
+	const selectedMenu = listMenu.find((menu)=>{
 		return menu.menu == activeMenu
 	});
 	
